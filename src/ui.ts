22b1f34ac5fe7500cb498722a16bb26dc53ed946
Jonathan <jonathanking.dev@gmail.com>
2024-02-15 09:41:21 +0100

    second commit

---
+++ src/ui.ts
@@ -0,0 +1,64 @@
+import {
+  comprobarFormacion,
+  comprobarValidez,
+  mostrarNombreBanco,
+  mostrarNumeroSucursal,
+  mostrarDigitoControl,
+  mostrarNumeroCuenta,
+} from "./motor";
+
+const cajaTexto = document.getElementById("cajaTexto");
+const botonBuscar = document.getElementById("button");
+const numeroBienFormado = document.getElementById("formado");
+const numeroValido = document.getElementById("valido");
+const nombreBanco = document.getElementById("banco");
+const numeroSucursal = document.getElementById("sucursal");
+const numeroControl = document.getElementById("numeroControl");
+const numeroCuenta = document.getElementById("numeroCuenta");
+
+export function initUI() {
+  //EVENTO QUE OCURRE CUANDO EL USUARIO PULSA EL BOTON
+  if (botonBuscar && botonBuscar instanceof HTMLButtonElement) {
+    botonBuscar.addEventListener("click", () => {
+      if (
+        cajaTexto &&
+        numeroBienFormado &&
+        cajaTexto instanceof HTMLInputElement
+      ) {
+        const numeroIban = cajaTexto.value;
+        analizarNumero(numeroIban);
+      }
+    });
+  }
+}
+
+const analizarNumero = (numero: string) => {
+  if (
+    numeroBienFormado &&
+    numeroValido &&
+    nombreBanco &&
+    numeroSucursal &&
+    numeroControl &&
+    numeroCuenta
+  ) {
+    const mensajeBienFormado = comprobarFormacion(numero);
+    numeroBienFormado.innerHTML = mensajeBienFormado;
+
+    const mensajeValidez = comprobarValidez(numero);
+    if (mensajeValidez) {
+      numeroValido.innerHTML = mensajeValidez;
+
+      const mostrarBanco = mostrarNombreBanco(numero);
+      nombreBanco.innerHTML = mostrarBanco;
+
+      const mostrarSucursal = mostrarNumeroSucursal(numero);
+      numeroSucursal.innerHTML = mostrarSucursal;
+
+      const mostrarControl = mostrarDigitoControl(numero);
+      numeroControl.innerHTML = mostrarControl;
+
+      const mostrarCuenta = mostrarNumeroCuenta(numero);
+      numeroCuenta.innerHTML = mostrarCuenta;
+    }
+  }
+};
